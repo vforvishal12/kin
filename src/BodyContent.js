8df83dec8d1b269fac3552371de831c6affca6b6
vforvishal12 <52970035+vforvishal12@users.noreply.github.com>
2024-09-24 12:57:22 +0530

    Update BodyContent.js

--- src/BodyContent.js
+++ src/BodyContent.js
@@ -19,7 +19,7 @@ function BodyContent(){
 
 
 var breakfastIndex  = Math.floor(Math.random() * 33) ;
-var mainMenuIndex  = Math.floor(Math.random() * ) ;
+var mainMenuIndex  = Math.floor(Math.random() * 62) ;
 
 var breakfast = ['Dal Tadka', 'Rajma Chawal', 'Aloo Gobhi', 'Chole', 'Paneer Butter Masala', 'Bhindi Masala', 'Baingan Bharta', 'Jeera Aloo', 'Kadhi Pakora', 'Palak Paneer', 'Mutter Paneer', 'Lauki Kofta', 'Aloo Matar', 'Gajar Matar', 'Tawa Sabzi', 'Methi Aloo', 'Pindi Chole', 'Stuffed Paratha', 'Aloo Baingan', 'Mix Veg', 'Dhuska', 'Fruit Salad', 'Tehri', 'Biryani', 'Pizza', 'Aaloo Paratha', 'Daal Bhaat Choka', 'Pulow', 'Chinnese', 'Crispy Chilly Baby Corn', 'Fasting', 'Dahi Bara', 'Bhindi', 'Aalo Dum', 'Aaloo Bhujiya','Idli','Doosa'];
 var mainMenu = ['Poha', 'Paratha - Bhujia', 'Chilla', 'Bread Butter Jam', 'Bread Chai', 'Sandwich', 'Poori Sabzi', 'Maggie', 'Aloo Paratha', 'Dhokla', 'Pav Bhaji'];
